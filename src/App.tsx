ee48286461dca5e3232727f203a2bc6337d24117
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-18 23:31:25 +0800

    defini a color

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { Button } from 'react-aria-components';
 
 function App() {
   return (
     <div className="p-5">
       <div className="text-3xl font-bold">Hello Vite</div>
-      <Button
-        onPress={() => alert('tested work')}
-        className="my-4 outline-none bg-blue-600 px-3 py-1 hover:bg-blue-500 text-white rounded-md border-transparent"
-      >
-        Tested
-      </Button>
+      <div className="w-2 h-2 bg-primary-blue"></div>
+      <div className="w-2 h-2 bg-primary-darkgray"></div>
+      <div className="w-2 h-2 bg-primary-midgray"></div>
+      <div className="w-2 h-2 bg-primary-lightgray"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: {
+        blue: '#2F80ED',
+        darkgray: '#4F4F4F',
+        midgray: '#828282',
+        lightgray: '#E0E0E0',
+      },
+      indicator: {
+        orange: '#F8B76B',
+        purple: '#8785FF',
+        red: '#EB5757',
+        yellow: '#F2C94C',
+      },
+      chats: {
+        50: '#FCEED3',
+        100: '#E5A443',
+        150: '#EEDCFF',
+        200: '#9B51E0',
+        250: '#D2F2EA',
+        300: '#43B78D',
+      },
+      sticker: {
+        50: '#E9F3FF',
+        100: '#FDCFA4',
+        150: '#F9E9C3',
+        200: '#AFEBDB',
+        250: '#CBF1C2',
+        300: '#CFCEF9',
+        450: '#F9E0FD',
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
